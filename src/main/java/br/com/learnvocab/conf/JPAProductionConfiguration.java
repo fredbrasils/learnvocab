36cdfe13e11ada19db5a500e1e3dffb12e7c28a3
Fred Brasil <fredswimmer@gmail.com>
2017-01-20 21:49:54 -0200

    ...

--- src/main/java/br/com/learnvocab/conf/JPAProductionConfiguration.java
+++ src/main/java/br/com/learnvocab/conf/JPAProductionConfiguration.java
@@ -23,7 +23,8 @@ public class JPAProductionConfiguration {
 		Properties props = new Properties();
 		props.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
 		props.setProperty("hibernate.show_sql", "true");
-		props.setProperty("hibernate.hbm2ddl.auto", "update");
+		//props.setProperty("hibernate.hbm2ddl.auto", "update");
+                props.setProperty("hibernate.hbm2ddl.auto", "create");
 		return props;
 	}
 
